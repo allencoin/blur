aa983a4b3b8483b6bbf499fc1c657a44594b5673
Allen Coin <allencoin@gmail.com>
2015-04-23 11:21:02 +0000

    this seems to work

--- blur1.rb
+++ blur1.rb
@@ -4,8 +4,8 @@ class Image
 	end
 
 	def output_image
-		@image.each do |i|
-			puts i
+		@image.each do |x|
+			puts x.join
 		end
 	end
 
@@ -14,12 +14,9 @@ row2 = [0, 1, 0, 0]
 row3 = [0, 0, 0, 1]
 row4 = [0, 0, 0, 0]
 
-image = Image.new([
-  row1,
-  row2,
-  row3,
-  row4
-])
+image = Image.new(
+	[row1, row2, row3, row4]
+)
 
 image.output_image
 
