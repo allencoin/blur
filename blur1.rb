6051b32fe6981235cd618d910b98d82f158ba52f
Allen Coin <allencoin@gmail.com>
2015-04-23 11:11:31 +0000

    blur1 displays each element on its own line?

---
+++ blur1.rb
@@ -0,0 +1,26 @@
+class Image
+	def initialize(image)
+		@image = image
+	end
+
+	def output_image
+		@image.each do |i|
+			puts i
+		end
+	end
+
+row1 = [0, 0, 0, 0]
+row2 = [0, 1, 0, 0]
+row3 = [0, 0, 0, 1]
+row4 = [0, 0, 0, 0]
+
+image = Image.new([
+  row1,
+  row2,
+  row3,
+  row4
+])
+
+image.output_image
+
+end
