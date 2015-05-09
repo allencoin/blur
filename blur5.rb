f57252d7eeb561d71652be3f9f138401e3de4103
Allen Coin <allencoin@gmail.com>
2015-05-09 08:15:06 +0000

    get one-neighboring-cell blur working

--- blur5.rb
+++ blur5.rb
@@ -9,31 +9,52 @@ class Image
 		@image.each do |row|
 			row.each do |column|
 			print column
-			# if (i == 2 && e == 3)
-			# 	print "XX"
-			# else
-			# 	print "YY"
-			# end
 		end
 			puts "\n"
 		end
 	end
 
 	def blur
-		blurred = []
-
+		ones = get_ones
 		@image.each_with_index do |row, i|
-			row.each_with_index do |element, e|
-				if element == 1
-					blurred << element
-				elsif element == 0
-					blurred << 0
+			row.each_with_index do |cell, j|
+				if ones.include?([i, j])
+					@image[i-1][j] = 1 unless i == 0
+					@image[i+1][j] = 1 unless i == (@image.length - 1)
+					@image[i][j-1] = 1 unless j == 0
+					@image[i][j+1] = 1 unless j == (row.length - 1)
 				end
-				print @blurred
 			end
-			puts "\n"
 		end
 	end
+
+	def get_ones
+		ones = []
+		@image.each_with_index do |row, i|
+			row.each_with_index do |cell, j|
+				if cell == 1
+					ones << [i, j]				
+				end
+			end
+		end
+		ones
+	end
+
+	# def blur
+	# 	blurred = []
+
+	# 	@image.each_with_index do |row, i|
+	# 		row.each_with_index do |element, e|
+	# 			if element == 1
+	# 				blurred << element
+	# 			elsif element == 0
+	# 				blurred << 0
+	# 			end
+	# 			print @blurred
+	# 		end
+	# 		puts "\n"
+	# 	end
+	# end
 
 	# def blur
 	# 	@image.each_with_index do |element, i|
@@ -52,12 +73,16 @@ class Image
 end
 
 image = Image.new([
-	[0, 0, 0, 0],
-	[0, 1, 0, 0],
-	[0, 0, 0, 1],
-	[0, 0, 0, 0]
+	[1, 0, 0, 1, 0],
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 1, 0],
+	[1, 0, 0, 0, 0]
 ])
 
 image.output_image
-
+puts "blurred v"
 image.blur
+puts "---"
+image.output_image
+puts "---"
+# puts image.get_ones
