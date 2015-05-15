368044bf24de47f3b830a49be6d2bdf135cd9ea7
Allen Coin <allencoin@gmail.com>
2015-05-15 12:16:38 +0000

    functional manhattan distance

--- blur5.rb
+++ blur5.rb
@@ -9,20 +9,23 @@ class Image
 		@image.each do |row|
 			row.each do |column|
 			print column
+			print " "
 		end
 			puts "\n"
 		end
 	end
 
-	def blur(distance)
-		ones = get_ones
-		@image.each_with_index do |row, i|
-			row.each_with_index do |cell, j|
-				if ones.include?([i, j])
-					@image[i-1][j] = 1 unless i == 0
-					@image[i+1][j] = 1 unless i == (@image.length - 1)
-					@image[i][j-1] = 1 unless j == 0
-					@image[i][j+1] = 1 unless j == (row.length - 1)
+	def blur(distance = 3)
+		distance.times do
+			ones = get_ones
+			@image.each_with_index do |row, i|
+				row.each_with_index do |cell, j|
+					if ones.include?([i, j])
+						@image[i-1][j] = 1 unless i == 0
+						@image[i+1][j] = 1 unless i == (@image.length - 1)
+						@image[i][j-1] = 1 unless j == 0
+						@image[i][j+1] = 1 unless j == (row.length - 1)
+					end
 				end
 			end
 		end
@@ -92,3 +95,7 @@ puts "---"
 image.output_image
 puts "---"
 # puts image.get_ones
+
+
+#manhattan distance Definition: The distance between two points measured along axes at right angles. 
+#In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|. 
